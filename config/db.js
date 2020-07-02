75e2657f41b63dfc0d2e44c420b6d679e8a3d581
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-02 13:52:14 +0530

    local mongodb to mongodb atlas connection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 let mongoose = require('mongoose');
 
-let dbUrl = 'mongodb://localhost:27017/structureDb';
+// let dbUrl = 'mongodb://localhost:27017/structureDb';
+let dbUrl = 'mongodb+srv://myuser:myuser@cluster0.1etyj.gcp.mongodb.net/structureDb'
 
-    mongoose.connect(dbUrl, { useNewUrlParser: true }, async (err) => {
+mongoose.connect(dbUrl, { useNewUrlParser: true }, async (err) => {
+    if (!err) {
+        console.log('db ok.');
+    }
+    else if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+    }
+});
 
-        if (err) {
-            console.log('db ok.');
-        }
-    });
 
 
 
